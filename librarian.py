c9aee8159c37bebc6e594ab15617108bf2a664be
Jitske-Bots <58811242+Jitske-Bots@users.noreply.github.com>
2020-04-26 15:57:31 +0200

    Librarian can add books

--- librarian.py
+++ librarian.py
@@ -1,3 +1,5 @@
+from books import Books
+
 def menu():
     while True:
         print("\\nPlease enter the number that stands before the actions you want to do\n[1] Look at all loans\n[2] Add a book\n[3] Add a customer\n[4] Make a backup\n[5] Restore data [6] Exit")
@@ -5,7 +7,13 @@ def menu():
         if userInput == "1":
             print("loans")
         elif userInput == "2":
-            print("add book")
+            print("Add book")
+            while True:
+                Books().addBook()
+                print("Do you want to add another book?\n[1] yes\n[2] no")
+                YorN = input()
+                if YorN == "2":
+                    break
         elif userInput == "3":
             print("add customer")
         elif userInput == "4":
